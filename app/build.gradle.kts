55619f82ce3239d91d6102fa327003da309f8882
Surya Saravanan <ssar0603@uni.sydney.edu.au>
2025-08-19 22:10:51 +1000

    Improved Performance for Low End Devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.datdt.scanningapp2D"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 13
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
